c41b98969ca12855217d524fd5d359dc80716a0c
Christy Heaton <Christy Heaton>
2017-02-26 06:24:23 -0800

    stage 1 with latest

--- resources/session08/mysite_stage_1/myblog/tests.py
+++ resources/session08/mysite_stage_1/myblog/tests.py
@@ -1,25 +1,23 @@
 from django.test import TestCase
-from django.contrib.auth.models import User
-
-from myblog.models import Category
 from myblog.models import Post
-
-
+# Create your tests here.
+from django.test import TestCase
+from django.contrib.auth.models import User
+from myblog.models import Category
 class PostTestCase(TestCase):
-    fixtures = ['myblog_test_fixture.json']
+    fixtures = ['myblog_test_fixture.json', ]
 
     def setUp(self):
         self.user = User.objects.get(pk=1)
 
+    # and this test method to the PostTestCase
     def test_string_representation(self):
         expected = "This is a title"
         p1 = Post(title=expected)
         actual = str(p1)
         self.assertEqual(expected, actual)
 
-
 class CategoryTestCase(TestCase):
-
     def test_string_representation(self):
         expected = "A Category"
         c1 = Category(name=expected)
